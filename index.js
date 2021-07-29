2cad29422e933c561a718466700c28d1d536c5be
abisola <abisola.jesukunle@oryx-wa.com>
2021-07-29 17:58:56 +0100

    measuration merge to main

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
+function measuration() {
+  console.log("This is a measuration function");
+}
 
 const integration = () => {
-	console.log('Something was integrated');
+  console.log("Something was integrated");
+};
+
+function trigonometry() {
+  console.log("This is a function for trigonometry");
 }
 
-function trigonometry(){
-	console.log('This is a function for trigonometry');
+function bearing(x, y) {
+  console.log(x * y);
 }
 
-function bearing(x,y){
-console.log(x*y)
+bearing();
+
+let surd = surds(6, 7);
+
+function surds(x, y) {
+  return x / y;
 }
+console.log(surd);
 
-bearing()
-
-let surd = surds(6, 7); 
-
-
-function surds(x, y){
-return x/y;
-}
-console.log(surd)
-
-function complexNumber(){
-	console.log("My branch is called Complex Branching");
+function complexNumber() {
+  console.log("My branch is called Complex Branching");
 }
 
 complexNumber();
-
-
